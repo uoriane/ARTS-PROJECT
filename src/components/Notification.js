8d1d532b204ff588a5eed4aeba839f9bd2ff6b77
Uwineza Oriane <o.uwineza@alustudent.com>
2026-01-09 10:19:27 +0200

    Delete src directory

--- src/components/Notification.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Notifications = () => (
-  <div style={{ background: "#fffae6", padding: "0.5em" }}>
-    <strong>Notifications:</strong> You have 2 new audit recommendations to review.
-  </div>
-);
-export default Notifications;
